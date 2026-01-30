a3f05512b9cbc755fd6b845591d2efdfe5b061b7
USER001 <lironlevy03@gmail.com>
2026-01-30 14:24:00 +0200

    commit1

--- app/src/main/java/com/example/avoda6/MainActivity.java
+++ app/src/main/java/com/example/avoda6/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.avoda6;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,17 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView tx;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        tx=findViewById(R.id.tx);
+    }
+
+    public void Clickme(View view) {
+        tx.setText("oh,yea,i've been clicked!");
     }
 }
